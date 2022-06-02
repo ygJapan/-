fa5b8942b32490127e6d0d1e6add371dc5981801
YugoDesk <yutaniyugo@gmail.com>
2022-06-02 17:16:12 +0900

    セクション6-37コミット

--- trello-clone/src/components/task/Tasks.jsx
+++ trello-clone/src/components/task/Tasks.jsx
@@ -4,31 +4,11 @@ import { Task } from './Task'
 
 export const Tasks = ({ taskList, setTaskList }) => {
     return (
-        // <div>
-        //         <DragDropContext>
-        //         <Droppable droppableId='droppable'>
-        //             {(provided) => {
-        //                 <div {...provided.droppableProps} ref={provided.innerRef}>
-        //                     {taskList.map((task) => (
-        //                         <div key={task.id}>
-        //                             <Task
-        //                                 task={task}
-        //                                 taskList={taskList}
-        //                                 setTaskList={setTaskList}
-        //                             />
-        //                         </div>
-        //                     ))}
-        //                     {provided.placeholder}
-        //                 </div>
-        //             }}
-        //         </Droppable>
-        //     </DragDropContext>
-        // </div>
         <div>
             <DragDropContext>
                 <Droppable droppableId='droppable'>
                     {(provided) => (
-                        <div { ...provided.droppableProps } ref={provided.innerRef}>
+                        <div {...provided.droppableProps} ref={provided.innerRef}>
                             {taskList.map((task) => (
                                 <div key={task.id}>
                                     <Task
