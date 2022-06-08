81cf28800a079bca1efb4b06e93caa7452252920
YugoDesk <yutaniyugo@gmail.com>
2022-06-08 15:58:12 +0900

    ウィジェットにTwitterライブラリを追加

--- twitter-clone/src/components/widget/Widget.jsx
+++ twitter-clone/src/components/widget/Widget.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
+} from 'react-twitter-embed';
+
 
 function Widget() {
   return (
-    <div>Widget</div>
+    <div className='widget'>
+      <div className='widget_input'>
+        <SearchIcon className='widget_seachIcon'/>
+        <input placeholder='キーワード検索' type='text'/>
+      </div>
+
+      <div className='widget_container'>
+        <h2>いまどうしてる？</h2>
+
+        <TwitterTweetEmbed
+          tweetId={'1425338261715652608'}
+        />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="ReactNativeJP"
+          options={{height: 400}}
+        />
+
+        <TwitterShareButton
+          url={'https://github.com/ygJapan'}
+          options={{ text: '#reactjs is awesome', via: 'reactjs' }}
+        />
+
+      </div>
+    </div>
   )
 }
 
