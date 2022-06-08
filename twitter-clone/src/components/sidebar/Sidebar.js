41355e6a15dd54d04d480ed2130cb4436c3beef0
YugoDesk <yutaniyugo@gmail.com>
2022-06-08 12:49:19 +0900

    マテリアルUIの導入

---
+++ twitter-clone/src/components/sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+        Sidebar
+        {/* ツイッターアイコン */}
+        <TwitterIcon className='sidebar_twitterIcon'/>
+
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+        {/* サイドバーオプション */}
+
+        {/* ツイートボタン */}
+
+    </div>
+  )
+}
