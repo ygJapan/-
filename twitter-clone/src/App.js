6a415cd53d3b2cc947b7fbdc25f32e8714e23958
YugoDesk <yutaniyugo@gmail.com>
2022-06-08 14:34:25 +0900

    タイムラインのヘッダー作成

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
+import Timeline from './components/timeline/Timeline';
+import Widget from './components/widget/Widget';
 
 function App() {
   return (
     <div className="app">
+      {/* サイドバー */}
       <Sidebar />
+      {/* タイムライン */}
+      <Timeline />
+      {/* ウィジェット */}
+      <Widget />
     </div>
   );
 }
---
+++ twitter-clone/src/components/timeline/Timeline.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Timeline.css";
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+        {/* ヘッダー */}
+            <div className='timeline_header'>
+                <h2>ホーム</h2>
+            </div>
+        {/* ツイートボックス */}
+            <div>
+            </div>
+
+        {/* ポスト */}
+            <div>
+            </div>
+
+
+    </div>
+  )
+}
+
+export default Timeline
