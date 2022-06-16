d6348c677c05b45be9e9549f6094e9a546ebf3c3
YugoDesk <yutaniyugo@gmail.com>
2022-06-16 17:41:42 +0900

    react-router-domの導入

--- SignUp/src/App.js
+++ SignUp/src/App.js
@@ -1,10 +1,18 @@
 import SignUp from './components/SignUp';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div style={{ margin: '2em' }}>
-      <SignUp />
-    </div>
+    <AuthProvider>
+      <div style={{ margin: '2em' }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
 
--- SignUp/src/components/SignUp.jsx
+++ SignUp/src/components/SignUp.jsx
@@ -1,45 +1,27 @@
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from '../firebase';
+import { useAuthContext } from '../context/AuthContext';
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
+  const { user } = useAuthContext();
   const handleSubmit = (event) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-  };
-
-  const handleChangeEmail = (event) => {
-    setEmail(event.currentTarget.value);
-  };
-  const handleChangePassword = (event) => {
-    setPassword(event.currentTarget.value);
+    auth.createUserWithEmailAndPassword(email.value, password.value);
   };
 
   return (
     <div>
-      <h1>ユーザ登録</h1>
+      <h1>ユーザ登録 {user.email}</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label>メールアドレス</label>
-          <input
-            name="email"
-            type="email"
-            placeholder="email"
-            onChange={(event) => handleChangeEmail(event)}
-          />
+          <input name="email" type="email" placeholder="email" />
         </div>
         <div>
           <label>パスワード</label>
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            onChange={(event) => handleChangePassword(event)}
-          />
+          <input name="password" type="password" placeholder="password" />
         </div>
         <div>
           <button>登録</button>
